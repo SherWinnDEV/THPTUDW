085eb9af5059a142162e50c934915f5743ea6a4f
mrtru <mrtru@TRUNGBOOK>
2023-10-12 18:00:35 +0700

    Add project files.

---
+++ THPTUDW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace THPTUDW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
